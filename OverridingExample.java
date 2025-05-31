fd8770529a73c79f2c33fcc084d73aadbff475cf
Rammohan78 <rammohan97048@gmail.com>
2025-05-31 14:06:40 +0530

    Add files via upload

---
+++ OverridingExample.java
@@ -0,0 +1,25 @@
+package xyz;
+class Animal{
+	void sound() {
+	System.out.println("Animls make sound");
+	}
+}
+class dog extends Animal{
+	void sound() {
+	System.out.println("Animls make sound");
+	}
+}
+
+
+
+
+
+
+public class OverridingExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
